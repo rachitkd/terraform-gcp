722d93c95c2538f3c582593f046b73b6e505ffa3
unknown <rachit.cisco@gmail.com>
2025-06-08 19:19:43 +0530

    U-1

--- vm-instance/main.tf
+++ vm-instance/main.tf
@@ -1,4 +1,16 @@
-resource "google_storage_bucket" "resource-aparna" {
-  name          = "rachit-bucket"
-  location      = "us-central1"
+resource "google_compute_instance" "default" {
+  name         = "aparna-vm-01"
+  machine_type = "e2-small"
+  zone         = "us-central1-f"
+
+  boot_disk {
+    initialize_params {
+      image = "rhel-9-v20250513"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
 }
