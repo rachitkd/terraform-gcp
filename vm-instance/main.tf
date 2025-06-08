c02ae3113b99e5ebd457738b462d409e5c00a2fb
unknown <rachit.cisco@gmail.com>
2025-06-09 01:27:42 +0530

    update-2

--- vm-instance/main.tf
+++ vm-instance/main.tf
@@ -2,8 +2,7 @@ resource "google_compute_instance" "default" {
   name         = "aparna-vm-01"
   machine_type = "e2-small"
   zone         = "us-central1-f"
-  Project = "red-abstraction-397916"
-  
+    
   boot_disk {
     initialize_params {
       image = "rhel-9-v20250513"
